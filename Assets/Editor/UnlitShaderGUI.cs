8b8c300ed37ee2c53586e5fcdba160a4d8c8e50a
azixMcAze <mathieu.leber@gmail.com>
2018-01-19 13:45:55 +0100

    Enable keyword NO_COLOR and NO_TEXTURE if needed

--- Assets/Editor/UnlitShaderGUI.cs
+++ Assets/Editor/UnlitShaderGUI.cs
@@ -8,5 +8,18 @@ public class UnlitShaderGUI : ShaderGUI
 	public override void OnGUI (MaterialEditor materialEditor, MaterialProperty[] properties)
 	{
 		base.OnGUI (materialEditor, properties);
+		Material mat = materialEditor.target as Material;
+		bool noTexture = mat.GetTexture("_MainTex") == null;
+		bool noColor = mat.GetColor("_Color") == Color.white;
+		EnableKeyword(mat, "NO_TEXTURE", noTexture);
+		EnableKeyword(mat, "NO_COLOR", noColor);
+	}
+
+	static void EnableKeyword(Material material, string keyword, bool enable)
+	{
+		if(enable)
+			material.EnableKeyword(keyword);
+		else
+			material.DisableKeyword(keyword);
 	}
 }
