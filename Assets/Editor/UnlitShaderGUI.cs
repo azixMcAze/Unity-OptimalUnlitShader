fe5cae7ba9d0d90e0e2ac90cdadc57b6902ca3af
azixMcAze <mathieu.leber@gmail.com>
2018-03-02 17:03:56 +0100

    Added toggles to force features

--- Assets/Editor/UnlitShaderGUI.cs
+++ Assets/Editor/UnlitShaderGUI.cs
@@ -16,6 +16,12 @@ public class UnlitShaderGUI : ShaderGUI
 	public enum MaterialFlags
 	{
 		NoMaskScaleOffset = 1 << 0,
+		AdvancedToggle = 1 << 1,
+		ForceTexture = 1 << 2,
+		ForceMask = 1 << 3,
+		ForceMaskScaleOffset = 1 << 4,
+		ForceColor = 1 << 5,
+		ForceCutoff = 1 << 6,
 	}
 
 	public const string RenderingModePropName = "_RenderingMode";
@@ -28,8 +34,15 @@ public class UnlitShaderGUI : ShaderGUI
 	public const string ZWritePropName = "_ZWrite";
 	public const string MaterialFlagsPropName = "_MaterialFlags";
 
-	static readonly GUIContent s_noMaskScaleOffsetLabel = new GUIContent("Same Tiling/Offset as Texture");
+	static readonly GUIContent s_noMaskScaleOffsetLabel = new GUIContent("Same Tiling & Offset as Texture");
+	static readonly GUIContent s_advancedLabel = new GUIContent("Advanced Options");
+	static readonly GUIContent s_forceTextureLabel = new GUIContent("Do not disable Texture");
+	static readonly GUIContent s_forceMaskLabel = new GUIContent("Do not disable Mask");
+	static readonly GUIContent s_forceMaskScaleOffsetLabel = new GUIContent("Do not disable Mask Tiling & Offset");
+	static readonly GUIContent s_forceColorLabel = new GUIContent("Do not disable Color");
+	static readonly GUIContent s_forceCutoffLabel = new GUIContent("Do not disable Alpha Cutoff");
 
+			
 	bool m_firstTime = false;
 
 	public override void OnGUI (MaterialEditor materialEditor, MaterialProperty[] properties)
@@ -74,6 +87,21 @@ public class UnlitShaderGUI : ShaderGUI
 		if((RenderingMode)renderingModeProp.floatValue == RenderingMode.Cutout)
 			DrawProperty(CutoffPropName, materialEditor, properties);
 
+		bool advancedToggle = GetMaterialFlag(materialFlags, MaterialFlags.AdvancedToggle);
+		advancedToggle = EditorGUILayout.Foldout(advancedToggle, s_advancedLabel);
+		materialFlags = SetMaterialFlag(materialFlags, MaterialFlags.AdvancedToggle, advancedToggle);
+		
+		if(advancedToggle)
+		{
+			EditorGUI.indentLevel++;
+			DrawFlagToggleProperty(s_forceTextureLabel, ref materialFlags, MaterialFlags.ForceTexture);
+			DrawFlagToggleProperty(s_forceMaskLabel, ref materialFlags, MaterialFlags.ForceMask);
+			DrawFlagToggleProperty(s_forceMaskScaleOffsetLabel, ref materialFlags, MaterialFlags.ForceMaskScaleOffset);
+			DrawFlagToggleProperty(s_forceColorLabel, ref materialFlags, MaterialFlags.ForceColor);
+			DrawFlagToggleProperty(s_forceCutoffLabel, ref materialFlags, MaterialFlags.ForceCutoff);
+			EditorGUI.indentLevel--;
+		}
+
 		materialFlagesProp.floatValue = (float)materialFlags;
 
 		EditorGUILayout.Space();
@@ -90,25 +118,37 @@ public class UnlitShaderGUI : ShaderGUI
 		return property;
 	}
 
+	static bool DrawFlagToggleProperty(GUIContent label, ref MaterialFlags allFlags, MaterialFlags flag)
+	{
+		bool toggle = GetMaterialFlag(allFlags, flag);
+		toggle = EditorGUILayout.Toggle(label, toggle);
+		allFlags = SetMaterialFlag(allFlags, flag, toggle);
+		return toggle;
+	}
+
 	void MaterialChanged(Material mat)
 	{
 		MaterialFlags materialFlags = (MaterialFlags)mat.GetInt(MaterialFlagsPropName);
 		
+		bool forceTextureFlag = GetMaterialFlag(materialFlags, MaterialFlags.ForceTexture);
 		bool noTexture = mat.GetTexture(MainTexPropName) == null;
 		Vector2 textureScale = mat.GetTextureScale(MainTexPropName);
 		Vector2 textureOffset = mat.GetTextureOffset(MainTexPropName);
 		bool noMask = mat.GetTexture(MaskPropName) == null;
+		bool forceMaskFlag = GetMaterialFlag(materialFlags, MaterialFlags.ForceMask);
 		Vector2 maskScale = mat.GetTextureScale(MaskPropName);
 		Vector2 maskOffset = mat.GetTextureOffset(MaskPropName);
 		bool sameMaskScale = maskScale == textureScale;
 		bool sameMaskOffset = maskOffset == textureOffset;
 		bool noMaskScaleOffsetFlag = GetMaterialFlag(materialFlags, MaterialFlags.NoMaskScaleOffset);
+		bool forceMaskScaleOffsetFlag = GetMaterialFlag(materialFlags, MaterialFlags.ForceMaskScaleOffset);
 		bool noMaskScaleOffset = noMaskScaleOffsetFlag || sameMaskScale && sameMaskOffset;
 		bool noColor = mat.GetColor(ColorPropName) == Color.white;
-		EnableKeyword(mat, "_TEXTURE_OFF", noTexture);
-		EnableKeyword(mat, "_MASK_OFF", noMask);
-		EnableKeyword(mat, "_MASK_SCALE_OFFSET_OFF", noMaskScaleOffset && !noMask);
-		EnableKeyword(mat, "_COLOR_OFF", noColor);
+		bool forceColorFlag = GetMaterialFlag(materialFlags, MaterialFlags.ForceColor);
+		EnableKeyword(mat, "_TEXTURE_OFF", noTexture && !forceTextureFlag);
+		EnableKeyword(mat, "_MASK_OFF", noMask && !forceMaskFlag);
+		EnableKeyword(mat, "_MASK_SCALE_OFFSET_OFF", noMaskScaleOffset && !noMask && !forceMaskScaleOffsetFlag);
+		EnableKeyword(mat, "_COLOR_OFF", noColor && !forceColorFlag);
 
 		RenderingMode renderingMode = (RenderingMode)mat.GetInt(RenderingModePropName);
 
@@ -129,7 +169,8 @@ public class UnlitShaderGUI : ShaderGUI
 				mat.SetInt(DstBlendPropName, (int)UnityEngine.Rendering.BlendMode.Zero);
 				mat.SetInt(ZWritePropName, 1);
 				bool alphaTest = mat.GetFloat(CutoffPropName) > 0f;
-				EnableKeyword(mat, "_ALPHATEST_ON", alphaTest);
+				bool forceCutoff = GetMaterialFlag(materialFlags, MaterialFlags.ForceCutoff);
+				EnableKeyword(mat, "_ALPHATEST_ON", alphaTest || forceCutoff);
 				mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.AlphaTest;
 				break;
 
